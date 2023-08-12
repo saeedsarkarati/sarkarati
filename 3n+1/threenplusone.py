b2f1d81ec62c33c0a04aef88503acc67e64441e9
saeedsarkarati <saeedsarkarati@gmail.com>
2023-08-12 11:01:50 +0430

    21-mordad-1402

--- 3n+1/threenplusone.py
+++ 3n+1/threenplusone.py
@@ -1,20 +1,20 @@
 a = []
-a.append(3)
-a.append(5)
-for i in range(7,1333333,2):
+la = 0
+for i in range(3,10000000000000+10000,1):
 	n = i
 	while n > 1:
-		if n < i:
-			break
+		# ~ if n < i:
+			# ~ break
 		if n in a:
 			break
+		a.append(n)
 		if n%2 == 1:
-			print (n, end = ' ')
-			a.append(n)
+			n =3 * n + 1
 		if n%2 == 0:
 			n = n //2
-		else: 
-			n =3 * n + 1
+	b = input()
 	# ~ if (i+1) % 40000 == 0:
 		# ~ print (i, a, len(a))
-	print(i)
+	print()
+	print(i, '-----', len(a), len(a) - la)
+	la = len(a)
